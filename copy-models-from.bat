3ffc4bca54dc1a26fb43d2442b310046b1f177d8
Pedro Agundez Fernandez <chernowi@gmail.com>
2025-04-30 10:57:14 +0200

    added bat

--- copy-models-from.bat
+++ copy-models-from.bat
@@ -1,1 +1,1 @@
-scp -r nct01026@alogin1.bsc.es:/home/nct/nct01026/ATCI-P1-BLOBTRACER/models "C:\Users\Pedro\Documents\MAI\ATCI\ATCI-P1-BLOBTRACER\" 
+scp -r nct01026@alogin1.bsc.es:/home/nct/nct01026/ATCI-P1-BLOBTRACER/models "C:\Users\Pedro\Documents\MAI\ATCI\ATCI-P1-BLOBTRACER\"
